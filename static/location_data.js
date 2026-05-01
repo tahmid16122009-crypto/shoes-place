5a9263af2540f8c2c4c4d5e7263ee877ed4c905e
tahmid16122009-crypto <tahmid16122009@gmail.com>
2026-05-02 01:09:31 +0600

    Create static/location_data.js

---
+++ static/location_data.js
@@ -0,0 +1,18 @@
+const locationData = {
+  "Dhaka": {
+    "Savar": ["Savar Union", "Birulia", "Ashulia"],
+    "Dhamrai": ["Dhamrai Union", "Kulla", "Balia"]
+  },
+  "Chittagong": {
+    "Patiya": ["Patiya Union", "Jiri", "Bara Uthan"],
+    "Raozan": ["Raozan Union", "Noapara"]
+  },
+  "Sylhet": {
+    "Beanibazar": ["Beanibazar Union", "Mathiura"],
+    "Golapganj": ["Golapganj Union"]
+  },
+  "Rajshahi": {
+    "Paba": ["Paba Union"],
+    "Bagha": ["Bagha Union"]
+  }
+};
